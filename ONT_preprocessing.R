4f8f94c567750b4575c7cda22a136dc965d617f9
Simone Maestri <simone.maestri@hotmail.it>
2020-06-25 19:10:46 +0200

    Add files via upload

--- ONT_preprocessing.R
+++ ONT_preprocessing.R
@@ -282,8 +282,8 @@ if (!dir.exists(d2_preprocessing)) {
       read_length <- ws
       cat(text = paste0("Mean read length (stdev) for sample BC", BC_val_curr, ": ", sprintf("%.0f", mean(ws)), " (", sprintf("%.0f", sd(ws)), ")"), file = logfile, sep = "\n", append = TRUE)
       cat(text = paste0("Mean read length (stdev) for sample BC", BC_val_curr, ": ", sprintf("%.0f", mean(ws)), " (", sprintf("%.0f", sd(ws)), ")"), sep = "\n")
-      cat(text = paste0("Now filtering out reads shorter than ", min_seq_length, " , longer than ", max_seq_length, " and with quality lower than ", min_qual, " for sample BC", BC_val_curr), file = logfile, sep = "\n", append = TRUE)
-      cat(text = paste0("Now filtering out reads shorter than ", min_seq_length, " , longer than ", max_seq_length, " and with quality lower than ", min_qual, " for sample BC", BC_val_curr), sep = "\n")
+      cat(text = paste0("Now filtering out reads shorter than ", min_seq_length, ", longer than ", max_seq_length, " and with quality lower than ", min_qual, " for sample BC", BC_val_curr), file = logfile, sep = "\n", append = TRUE)
+      cat(text = paste0("Now filtering out reads shorter than ", min_seq_length, ", longer than ", max_seq_length, " and with quality lower than ", min_qual, " for sample BC", BC_val_curr), sep = "\n")
       system(paste0("cat ", d2_preprocessing, "/BC", BC_val_curr, "_tmp1.fastq | ", NANOFILT, " -l ", format(min_seq_length, scientific = F), " --maxlength ", format(max_seq_length, scientific = F), " -q ", min_qual, " --logfile ", d2_preprocessing, "/BC", BC_val_curr, "_NanoFilt.log > ", d3, "/BC", BC_val_curr, ".fastq"))
       
       system(paste0(SEQTK, " seq -A ", d3, "/BC", BC_val_curr, ".fastq > ", d3, "/BC", BC_val_curr, ".fasta"))
